29b3efc01007699f42bbda52ce70e867a1f5dc52
Jason Fager <jfager@gmail.com>
2011-02-11 18:38:07 -0500

    minor profiler-driven optimizations for StringBitChecker

--- src/main/java/io/prelink/critbit/StringBitChecker.java
+++ src/main/java/io/prelink/critbit/StringBitChecker.java
@@ -4,25 +4,26 @@ package io.prelink.critbit;
  * Most of this based on a similar implementation from
  * (https://github.com/rkapsi/patricia-trie).
  */
-public class StringBitChecker implements BitChecker<String> {
+public final class StringBitChecker implements BitChecker<String> {
     private static final int MSB = 1 << Character.SIZE-1;
-    public boolean isSet(String key, int i) {
-        if (key == null) return false;
+    public boolean isSet(final String key, final int i) {
+        if(i >= bitLength(key)) {
+            return false;
+        }
 
-        int charIndex = (int)(i / Character.SIZE);
-        if (charIndex >= key.length()) return false;
-
-        int bit = (int)(i % Character.SIZE);
-        int mask = (MSB >>> bit);
+        final int charIndex = (int)(i / Character.SIZE);
+        final int bit = (int)(i % Character.SIZE);
+        final int mask = (MSB >>> bit);
         return (key.charAt(charIndex) & mask) != 0;
     }
 
-    private static final int indexFor(int charIndex, int bitIndex) {
+    private static int indexFor(final int charIndex,
+                                final int bitIndex) {
         return (charIndex * Character.SIZE) +
                (Integer.numberOfLeadingZeros(bitIndex) - Character.SIZE);
     }
 
-    public int firstDiff(String k1, String k2) {
+    public int firstDiff(final String k1, final String k2) {
         String shorter = k1.length() < k2.length() ? k1 : k2;
         int ci;
         for(ci = 0; ci < shorter.length(); ci++) {
@@ -45,11 +46,11 @@ public class StringBitChecker implements BitChecker<String> {
         return -1; //they are the same
     }
 
-    public int bitLength(String key) {
+    public int bitLength(final String key) {
         return key.length() * Character.SIZE;
     }
 
-    public boolean startsWith(String key, String prefix) {
+    public boolean startsWith(final String key, final String prefix) {
         return key.startsWith(prefix);
     }
 }
