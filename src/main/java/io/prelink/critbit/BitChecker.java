8c48a3b4df7fd4de4cfd3c3590e94ca83f0a9e2f
Jason Fager <jfager@gmail.com>
2011-02-21 14:36:40 -0500

    remove BitChecker code

--- src/main/java/io/prelink/critbit/BitChecker.java
+++
@@ -1,8 +0,0 @@
-package io.prelink.critbit;
-
-public interface BitChecker<K> {
-    boolean isSet(K key, int i);
-    int firstDiff(K k1, K k2);
-    int bitLength(K key);
-    boolean startsWith(K key, K prefix);
-}
